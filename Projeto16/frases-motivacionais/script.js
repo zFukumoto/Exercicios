e7a6659bcadbc20ee981c9b332dde704d037e342
zFukumoto <gabriel.fukumoto@Outlook.com>
2026-03-28 19:42:48 -0300

    Adicionado Script que pega uma Frase aleatório das disponíveis e mostra na tela

---
+++ Projeto16/frases-motivacionais/script.js
@@ -0,0 +1,5 @@
+function gerarFrase() {
+    const lista = ["Acredite em si mesmo e você será imparável.", "Cada dia é uma nova oportunidade para brilhar.", "Se você pode sonhar, pode realizar.", "A persistência realiza o impossível.", "A jornada mais longa começa com um único passo.", "Grandes coisas nunca vêm da zona de conforto.", "Mude seus pensamentos e você mudará seu mundo.", "Não desista, supere.", "Tudo parece impossível até que seja feito.", "A força está dentro de você, apenas a liberte."];
+    const item = lista[Math.floor(Math.random() * lista.length)];
+    document.getElementById("fraseMotivacional").innerHTML = item;
+}
