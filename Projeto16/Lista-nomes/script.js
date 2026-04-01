3f51470b321b64a8e012f01a849480088522f309
zFukumoto <gabriel.fukumoto@Outlook.com>
2026-03-31 21:41:34 -0300

    Adicionado Botão de Pesquisa de Nomes - Projeto finalizado

--- Projeto16/Lista-nomes/script.js
+++ Projeto16/Lista-nomes/script.js
@@ -1,9 +1,20 @@
 let nomes = ["gabriel", "maria", "joão", "ana", "pedro", "lucas", "mariana", "carlos", "juliana", "fernando", "carla", "ricardo", "sandra", "roberto", "paula", "andré", "marcos", "adriana", "bruno", "ricardo"];
-function carregarNomes(){
-    let itensLista = '';
+function pesquisarNome(){
+    let nomePesquisa = document.getElementById('campoNome').value
+    let itensLista = ''
     for(indice in nomes){
-        let nome = nomes[indice];
+        let nome = nomes[indice]
+        if(nomePesquisa == nome){
         itensLista += `<li class="list-group-item">${nome}</li>`;
+        }
     }
     document.getElementById('lista').innerHTML = itensLista;
-};
+}
+function carregarNomes(){
+    let itensLista = ''
+    for(indice in nomes){
+        let nome = nomes[indice]
+        itensLista += `<li class="list-group-item">${nome}</li>`;
+    }
+    document.getElementById('lista').innerHTML = itensLista
+}
